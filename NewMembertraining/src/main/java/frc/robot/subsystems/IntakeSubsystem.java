7b87a1a9f9e833ef1c879d0060df034eaf7b3550
Sunjot Ghotra <sunjotsinghg@gmail.com>
2026-01-24 11:27:52 -0500

    Working on intakeconstants in constants.java

--- NewMembertraining/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ NewMembertraining/src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -5,6 +5,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class IntakeSubsystem extends SubsystemBase {
     /** Creates a new IntakeSubsystem */
