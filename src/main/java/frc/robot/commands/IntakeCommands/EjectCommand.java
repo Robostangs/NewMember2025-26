e64971aa3914456324d5a8be5b9f4a854e92ba0b
Wowthatcool <suhasaradhya271@gmail.com>
2026-02-04 20:34:59 -0500

    Updated & imported files

--- src/main/java/frc/robot/commands/IntakeCommands/EjectCommand.java
+++ src/main/java/frc/robot/commands/IntakeCommands/EjectCommand.java
@@ -1,4 +1,4 @@
-package frc.robot.commands.IntakeCommands;
+package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
@@ -7,17 +7,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.subsystems.IntakeSubsystem;
 
 public class EjectCommand extends Command {
   
-    private final IntakeSubsystem m_intakeSubsystem;
-    
-
-    private EjectCommand() {
-        this.m_intakeSubsystem = IntakeSubsystem.getInstance();
-        addRequirements(m_intakeSubsystem);
-    };
 
     @Override
     public void initialize() {
@@ -32,7 +24,7 @@ public class EjectCommand extends Command {
     @Override
     public boolean isFinished() {
         return false;
-    }
+
 
 
 }
