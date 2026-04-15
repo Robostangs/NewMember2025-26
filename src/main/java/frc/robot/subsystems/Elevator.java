3c0e77356c62598c93df98b918608b6032398506
Wowthatcool <suhasaradhya271@gmail.com>
2026-04-15 19:22:04 -0400

    added more libraried

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -3,8 +3,9 @@ package frc.robot.subsystems;
 import com.revrobotics.sim.SparkMaxSim;
 import com.revrobotics.spark.SparkLowlevel;
 import com.revrobotics.spark.SparkMax;
-
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Elevator {
     private static Elevator mInstance;
     private final DCMotor m_elevatorGearbox = DCMotor.getNEO(1);
