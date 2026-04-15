1b38b815ccc15fa828d872b7f358b3f089c9bf1b
Wowthatcool <suhasaradhya271@gmail.com>
2026-04-15 19:16:40 -0400

    Started working on elevator simulator
    elevator sim

---
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.sim.SparkMaxSim;
+import com.revrobotics.spark.SparkLowlevel;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+public class Elevator {
+    private static Elevator mInstance;
+    private final DCMotor m_elevatorGearbox = DCMotor.getNEO(1);
+    private final SparkMaxSim m_motor = new SparkMax(13, SparkLowlevel.MotorType.kBrushless);
+
+}
